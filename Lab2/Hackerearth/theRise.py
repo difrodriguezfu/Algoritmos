8faff85172afb5c1b55f358672af75a0ec03a613
Fernando Rodriguez <diegodfrf@gmail.com>
2018-09-23 23:00:11 -0500

    The Rise.py

--- Lab2/Hackerearth/theRise.py
+++ Lab2/Hackerearth/theRise.py
@@ -8,5 +8,17 @@ for i in range(N):
         A[j] = A[j-1]
         j = j-1
     A[j] = temp
+Z = []
+V = []
+for i in range(N):
+    if A[i] % 2 == 0:
+        Z.append(A[i])
+    else:
+        V.append(A[i])
 
-print A
+for i in range(len(Z)):
+    print Z[i],
+print sum(Z),
+for i in range(len(V)):
+    print V[i],
+print sum(V)
